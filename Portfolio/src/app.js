61e6c374339f970f0dae3916d21bd2ef896c6a8d
Toan <toanh@usc.edu>
2022-06-06 13:54:48 -0700

    heroku added

--- Portfolio/src/app.js
+++ Portfolio/src/app.js
@@ -2,15 +2,18 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000,()=> {
-    console.log("oh yeahhh")})
 
 app.use(express.static("/Users/toanhuynh/Desktop/my_personal_website/Portfolio"))
+
 
 app.get('*',(req,res)=>{
     res.send("<h1 style ='text-allign:center'> 404 PAGE NOT FOUND <h1>")
 }
 )
+
+app.listen(port,()=> {
+    console.log("oh yeahhh")})
 
 
